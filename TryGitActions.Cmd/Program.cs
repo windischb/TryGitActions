ec8c758e70dee0cc1f9bb3fd4336c53fcda6f59a
Bernhard Windisch <bernhard.windisch@doob.at>
2019-10-23 10:32:20 +0200

    Add project files.

---
+++ TryGitActions.Cmd/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace TryGitActions.Cmd
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
